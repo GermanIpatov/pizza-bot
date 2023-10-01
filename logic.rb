12bf593115ef77f2dd384dd2e52d0c857a51cbd2
GermanIpatov <germanipatov6@gmail.com>
2023-10-02 00:53:37 +0300

    refactor

--- logic.rb
+++ logic.rb
@@ -1,38 +1,33 @@
 require_relative "Parser.rb"
 
 class PizzaBot
-    attr_accessor(:size_of_field_x, :size_of_field_y, :coordinates)
-
-    def initialize(size_of_field_x, size_of_field_y, coordinates)
-        @size_of_field_x = size_of_field_x
-        @size_of_field_y = size_of_field_y
-        @coordinates = coordinates
         
-    end
 
     def self.get_navigation(given_str)                           
         size_of_field = Parser.get_size_of_field(given_str)
-        @size_of_field_x, @size_of_field_y = size_of_field[0], size_of_field[1]
-
+        @size_of_field_x, @size_of_field_y = size_of_field
+        p @size_of_field_x, @size_of_field_y
         @coordinates = Parser.get_coordinates_arr(given_str)
     end
 
     def self.deliver_pizza
-        current_coordinates = [0, 0]
-        original_coordinates = [0, 0]
-        path = ""
-        @coordinates.each do |i|
-            until current_coordinates[0] == i[0]
-                i[0] > current_coordinates[0] ? current_coordinates[0] += 1 : current_coordinates[0] -= 1
-            end
-            until current_coordinates[1] == i[1]
-                i[1] > current_coordinates[1] ? current_coordinates[1] += 1 : current_coordinates[1] -= 1
-            end
-            original_coordinates[0] < current_coordinates[0] ? path += "E" * (current_coordinates[0] - original_coordinates[0]).abs : path += "W" * (original_coordinates[0] - current_coordinates[0]).abs
-            original_coordinates[1] < current_coordinates[1] ? path += "N" * (current_coordinates[1] - original_coordinates[1]).abs : path += "S" * (original_coordinates[1] - current_coordinates[1]).abs
-            original_coordinates[0], original_coordinates[1] = current_coordinates[0], current_coordinates[1]
-            path += "D"
+        @current = [0, 0]
+        
+        @path = ""
+        @coordinates.each{|i| aim(i)}
+        @path                                    
+    end
+
+    def self.aim(goal)
+        original = @current[0], @current[1]    
+        until @current[0] == goal[0] 
+            goal[0] > @current[0] ? @current[0] += 1 : @current[0] -= 1
         end
-        path
+        until @current[1] == goal[1]
+            goal[1] > @current[1] ? @current[1] += 1 : @current[1] -= 1
+        end
+        original[0] < @current[0] ? @path += "E" * (@current[0] - original[0]).abs : @path += "W" * (original[0] - @current[0]).abs
+        original[1] < @current[1] ? @path += "N" * (@current[1] - original[1]).abs : @path += "S" * (original[1] - @current[1]).abs
+        @path += "D"
     end
 end
--- main.rb
+++ main.rb
@@ -3,5 +3,5 @@ require_relative "logic.rb"
 p "Enter field and coordinates (format: 1x2(x1, y1)(x2, y2)) - "
 coordinates_string = gets.chomp
 PizzaBot.get_navigation(coordinates_string)
-p "Result is: "
+
 p PizzaBot.deliver_pizza
